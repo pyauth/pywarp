ba430bbcdceda69edfddf1916d98d4426518a498
Ranieri Althoff <ranisalt@gmail.com>
2019-04-01 19:19:04 -0300

    Add support for custom displayName and icon user attributes

--- tests/pywarp_test.py
+++ tests/pywarp_test.py
@@ -4,3 +4,14 @@ def test_get_registration_options(fake, rp):
 
     user = opts['user']
     assert user['name'] == email
+
+    display_name = fake.name()
+    icon = fake.image_url()
+    opts = rp.get_registration_options(
+        email=email, display_name=display_name, icon=icon
+    )
+
+    user = opts['user']
+    assert user['name'] == email
+    assert user['displayName'] == display_name
+    assert user['icon'] == icon

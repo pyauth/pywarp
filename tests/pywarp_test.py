e69dfaf90b65fd9fef86917bebe14cea76e11e5d
Ranieri Althoff <ranisalt@gmail.com>
2019-04-01 18:23:13 -0300

    Start improving tests

--- tests/pywarp_test.py
+++ tests/pywarp_test.py
@@ -1,4 +1,6 @@
-def test_pywarp(fake, rp):
+def test_get_registration_options(fake, rp):
     email = fake.email()
     opts = rp.get_registration_options(email=email)
-    print(email, opts)
+
+    user = opts['user']
+    assert user['name'] == email
